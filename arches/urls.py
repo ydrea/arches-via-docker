faa326f965930519e2907f915be563a6738fcad9
ydrea <ydrea.wrld@gmail.com>
2024-11-10 10:16:04 +0100

    no langs no i18n_patterns

--- arches/urls.py
+++ arches/urls.py
@@ -1,7 +1,7 @@
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
+# from django.conf.urls.i18n import i18n_patterns
 from django.urls import path, include
 
 urlpatterns = []
@@ -10,9 +10,9 @@ urlpatterns.append(path('', include('arches.urls')))
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Wrap urlpatterns with i18n_patterns only if this is the main URLconf
-if settings.ROOT_URLCONF == __name__:
-    if settings.SHOW_LANGUAGE_SWITCH is True:
-        urlpatterns = i18n_patterns(*urlpatterns)
+# if settings.ROOT_URLCONF == __name__:
+#     if settings.SHOW_LANGUAGE_SWITCH is True:
+#         urlpatterns = i18n_patterns(*urlpatterns)
 
-    # Add route for i18n support
-    urlpatterns.append(path("i18n/", include("django.conf.urls.i18n")))
+#     # Add route for i18n support
+#     urlpatterns.append(path("i18n/", include("django.conf.urls.i18n")))
