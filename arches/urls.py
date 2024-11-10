a614761431ab9fdc46427d6b3cb303c650ea4224
ydrea <ydrea.wrld@gmail.com>
2024-11-10 06:40:18 +0100

    FIX urlpatterns

--- arches/urls.py
+++ arches/urls.py
@@ -1,22 +1,25 @@
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.urls import include, path
 
+# Initial URL patterns, containing project-level URLs
 urlpatterns = [
-    # project-level urls
+    # Add project-specific URLs here
 ]
 
-# Ensure Arches core urls are superseded by project-level urls
+# Include Arches core URLs
 urlpatterns.append(path('', include('arches.urls')))
 
 # Adds URL pattern to serve media files during development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# Only handle i18n routing in active project. This will still handle the routes provided by Arches core and Arches applications,
-# but handling i18n routes in multiple places causes application errors.
+# Wrap urlpatterns with i18n_patterns only if this is the main URLconf
 if settings.ROOT_URLCONF == __name__:
     if settings.SHOW_LANGUAGE_SWITCH is True:
+        # Use i18n_patterns at the top level, wrapping all urlpatterns
         urlpatterns = i18n_patterns(*urlpatterns)
 
+    # Add route for i18n support
     urlpatterns.append(path("i18n/", include("django.conf.urls.i18n")))
